68573bbc7605c7a5046e46ef07ebf82787380938
RafaKHLi <rafaph2621@gmail.com>
2026-04-06 16:36:36 +0800

    Add app/page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import { useEffect } from 'react'
+
+// The main SnapVisa app is served as a client-side page
+// All the app logic lives in the HTML/JS loaded here
+export default function Home() {
+  return (
+    <div id="snapvisa-root" />
+  )
+}
