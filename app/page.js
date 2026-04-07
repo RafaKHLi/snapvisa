d7b4068d678b953e9d983e0c99e392cdc00bdd82
RafaKHLi <rafaph2621@gmail.com>
2026-04-07 08:35:00 +0800

    Fix: redirect / to /index.html

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-'use client'
-import { useEffect } from 'react'
+import { redirect } from 'next/navigation'
 
-// The main SnapVisa app is served as a client-side page
-// All the app logic lives in the HTML/JS loaded here
 export default function Home() {
-  return (
-    <div id="snapvisa-root" />
-  )
+  redirect('/index.html')
 }
